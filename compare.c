c47dc557a6f7846af89e81d6fc4faab6a992b946
bmills23 <millsbryang@gmail.com>
2024-06-05 22:08:18 -0600

    Practicing

---
+++ compare.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Prototype
+int getInt(int);
+
+int main(void)
+{
+    int x = get
+}
+
+int getInt()
+{
+    char message[100];
+    scanf("%d", )
+
+    strcpy(message, "Enter value of ");
+    strcpy(message)
+    printf("Enter the value of : ");
+    scanf("%s", name);
+    printf("Hello, %s!\n", name);
+    return 0;
+}
